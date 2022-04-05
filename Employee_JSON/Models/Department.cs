65856be26e55fe8579cc6392d1790ec7d8b8a5e8
Nurlan <99797183+hnurlan7@users.noreply.github.com>
2022-04-05 13:07:45 +0400

    Add files via upload

---
+++ Employee_JSON/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_JSON.Models
+{
+    class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Employee> _employees;
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name; 
+            _employees = new List<Employee>();
+        }
+    }
+}
---
+++ Employee_JSON/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_JSON.Models
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+        public string ShowInfo()
+        {
+            return $"Id: {Id} - Name: {Name} - Salary: {Salary}";
+        }
+    }
+}
